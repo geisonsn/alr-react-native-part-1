7c47db6fc86b36607c995b87bda5b1f6958735e7
Geison Santos <geison.sn@gmail.com>
2019-01-14 14:02:17 -0400

    Configura a listagem de cursos

--- App.js
+++ App.js
@@ -7,43 +7,38 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Text, 
+  View, 
+  Image, 
+  Dimensions, 
+  FlatList} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const width = Dimensions.get('screen').width;
+
+const fotos = [
+  {id: 1, usuario: 'Raphael'},
+  {id: 2, usuario: 'Alberto'},
+  {id: 3, usuario: 'Vítor'},
+];
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <FlatList 
+        style={{marginTop: 20}}
+        data={fotos}
+        keyExtractor={item => item.id.toString()}
+        renderItem={ ({item}) =>
+          <View>
+            <Text>{item.usuario}</Text>
+            <Image 
+              source={require('./resources/img/alura.jpg')} 
+              style={{width:width, height:width}} />
+          </View>
+        }
+        />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
